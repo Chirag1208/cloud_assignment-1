790aa6485129adf91eac90aac4ad1fc9d9e8ea2f
Chirag1208 <50573382+Chirag1208@users.noreply.github.com>
2020-10-12 21:10:16 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,14 +1,7 @@
-# Program to display calendar of the given month and year
 
-# importing calendar module
 import calendar
 
-yy = 2020  # year
-mm = 10    # month
+yy = 2020 
+mm = 10 
 
-# To take month and year input from the user
-# yy = int(input("Enter year: "))
-# mm = int(input("Enter month: "))
-
-# display the calendar
 print(calendar.month(yy, mm))
